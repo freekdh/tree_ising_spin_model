a0658f9c62d364b1166f81f24c7faaf17461a3a6
Freek <freek.de.haas@1qbit.com>
2021-09-14 16:55:20 +0200

    rename test file

--- tests/test_min_configuration.py
+++ tests/test_min_configuration.py
@@ -4,7 +4,7 @@ from tree_ising.main import solve_ising_problem
 
 
 @pytest.mark.parametrize("root_node", [0, 1, 3])
-def test_get_min_energy(problem_loader_from_file, root_node):
+def test_solve_ising_problem(problem_loader_from_file, root_node):
 
     ising_problem = problem_loader_from_file.load_problem(
         file_path="tests/test_problems/input_test_problem.txt", root_node=root_node
